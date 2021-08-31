82e2f5884588166ecfa463c3ce2e1bd6540f8bc0
unknown <tvtrong12a239@gmail.com>
2021-08-31 22:20:41 +0700

    upgrade file

---
+++ Web/Test/js/myScript.js
@@ -0,0 +1,8 @@
+function pheptinh(){
+    var a = 5;
+    var b = 6;
+    document.getElementById("cong").innerHTML = a + b ;
+    document.getElementById("tru").innerHTML = a - b ;
+    document.getElementById("nhan").innerHTML = a * b ;
+    document.getElementById("chia").innerHTML = a / b ;
+}
